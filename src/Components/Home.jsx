49f1f54f45b5a3e4b341db7a1562638125c033e8
hegelminniti <hegelminniti@gmail.com>
2024-10-14 10:34:05 +0200

    Started hero section homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,29 @@
 import { Navbar } from "./Navbar/Navbar"
+import '../Styles/home.css'
+import '../Styles/navbar.css'
 
 export function Home(){
     return(
+
         <>
-        <Navbar></Navbar>
+
+        <div className="navContainer">
+         <Navbar></Navbar>
+        </div>
+        <div className="mainContainer">
+            <div className="heroMain">
+                <img className="heroMainImg" src="../../public/Hero_Gioia_Tauro.jpg" alt="Lungomare di Gioia Tauro" />
+
+                <div className="heroMainText">
+                    <div className="heroMainTitle">
+                        Stella Polare
+                    </div>
+                    <div className="heroMainDescription">
+                       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque veritatis consequuntur provident praesentium? Doloremque architecto magnam dignissimos nihil hic repudiandae?
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
